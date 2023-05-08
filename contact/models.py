1b45a6f2edf00dbcb03ba9d1ad9eaa00d8830d5f
alexzanotti <>
2023-05-08 19:35:06 +0000

    updated contact model with status and dates/times

--- contact/models.py
+++ contact/models.py
@@ -4,9 +4,24 @@ from django.db import models
 
 
 class Contact(models.Model):
+    STATUS_CHOICES = (
+        ('New', 'New'),
+        ('Assigned', 'Assigned'),
+        ('In Progress', 'In Progress'),
+        ('Pending', 'Pending'),
+        ('Resolved', 'Resolved'),
+    )
+
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.TextField()
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='New',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} - {self.email}"
