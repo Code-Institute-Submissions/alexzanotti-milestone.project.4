42a7e56f051203ee61ee57add5cb2642aaa77b16
alexzanotti <>
2023-05-10 20:47:21 +0000

    added post commenting functionality

--- community/urls.py
+++ community/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.community, name='community'),
-    path('posts/category/<int:category_id>/',
+    path('category/<int:category_id>/',
          views.community_category, name='community_category'),
+    path('posts/category/<int:category_id>/',
+         views.community_category, name='posts_by_category'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/<int:post_id>/add_comment/',
+         views.add_comment, name='add_comment'),
 ]
--- community/views.py
+++ community/views.py
@@ -5,12 +5,6 @@ from django.db.models import Count, Max
 
 # Create your views here.
 
-"""
-def community(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'community/community.html', {'posts': posts})
-"""
-
 
 def community(request):
     categories = Category.objects.annotate(
@@ -24,3 +18,30 @@ def community_category(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     posts = Post.objects.filter(category=category)
     return render(request, 'community/community_category.html', {'category': category, 'posts': posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = Comment.objects.filter(post=post).order_by('created_at')
+    return render(request, 'community/post_detail.html', {'post': post, 'comments': comments})
+
+
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user.profile
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = CommentForm()
+
+    context = {
+        'form': form,
+        'post': post,
+    }
+    return render(request, 'community/add_comment.html', context)
