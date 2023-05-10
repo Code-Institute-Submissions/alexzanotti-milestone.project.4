5e3a32116f3fe37d4d11556461b1426ed7ba908c
alexzanotti <>
2023-05-10 20:13:35 +0000

    added profile forms and added to profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from .forms import ProfileUpdateForm
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Profile
@@ -6,5 +7,16 @@ from .models import Profile
 @login_required
 def profile(request):
     profile = get_object_or_404(Profile, profile_user=request.user)
-    context = {'profile': profile}
+
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=profile)
+
+        if form.is_valid():
+            form.save()
+            # Add a success message or redirect as needed
+
+    else:
+        form = ProfileUpdateForm(instance=profile)
+
+    context = {'form': form}
     return render(request, 'profiles/profile.html', context)
