e98725a05f96a2825d99896eeebeb8452ba1bf4f
alexzanotti <>
2023-05-10 20:34:12 +0000

    updated community page and added category page

--- community/urls.py
+++ community/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.community, name='community'),
+    path('posts/category/<int:category_id>/',
+         views.community_category, name='community_category'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category, Post, Comment
 from .forms import CategoryForm, PostForm, CommentForm
+from django.db.models import Count, Max
 
 # Create your views here.
 
-
+"""
 def community(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'community/community.html', {'posts': posts})
+"""
+
+
+def community(request):
+    categories = Category.objects.annotate(
+        num_posts=Count('post'),
+        latest_post_date=Max('post__created_at')
+    )
+    return render(request, 'community/community.html', {'categories': categories})
+
+
+def community_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    posts = Post.objects.filter(category=category)
+    return render(request, 'community/community_category.html', {'category': category, 'posts': posts})
