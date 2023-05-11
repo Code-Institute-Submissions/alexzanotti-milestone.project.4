ab9b0ae8f72088a6c8a9df1a5ee9750f69e0b005
alexzanotti <>
2023-05-11 06:50:14 +0000

    added functionality to the community management page

--- community/urls.py
+++ community/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+app_name = 'community'
 
 urlpatterns = [
     path('', views.community, name='community'),
@@ -11,4 +13,20 @@ urlpatterns = [
     path('posts/<int:post_id>/add_comment/',
          views.add_comment, name='add_comment'),
     path('add_post/', views.add_post, name='add_post'),
+    path('community_management/', views.community_management,
+         name='community_management'),
+    path('community_management/add_category/',
+         views.add_category, name='add_category'),
+    path('community_management/edit_category/<int:category_id>/',
+         views.edit_category, name='edit_category'),
+    path('community_management/delete_category/<int:category_id>/',
+         views.delete_category, name='delete_category'),
+    path('community_management/edit_post/<int:post_id>/',
+         views.edit_post, name='edit_post'),
+    path('community_management/delete_post/<int:post_id>/',
+         views.delete_post, name='delete_post'),
+    path('community_management/delete_comment/<int:comment_id>/',
+         views.delete_comment, name='delete_comment'),
+    path('community_management/edit_comment/<int:comment_id>/',
+         views.edit_comment, name='edit_comment'),
 ]
--- community/views.py
+++ community/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category, Post, Comment
 from .forms import CategoryForm, PostForm, CommentForm
 from django.db.models import Count, Max
+from django.urls import reverse
 
 # Create your views here.
 
@@ -33,7 +35,7 @@ def add_post(request):
             post = form.save(commit=False)
             post.author = request.user.profile
             post.save()
-            return redirect('post_detail', post_id=post.id)
+            return redirect(reverse('community:post_detail', args=[post.id]))
     else:
         form = PostForm()
 
@@ -53,7 +55,7 @@ def add_comment(request, post_id):
             comment.author = request.user.profile
             comment.post = post
             comment.save()
-            return redirect('post_detail', post_id=post.id)
+            return redirect(reverse('community:post_detail', args=[post.id]))
     else:
         form = CommentForm()
 
@@ -62,3 +64,88 @@ def add_comment(request, post_id):
         'post': post,
     }
     return render(request, 'community/add_comment.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def community_management(request):
+    categories = Category.objects.all()
+    posts = Post.objects.all()
+    comments = Comment.objects.all()
+    context = {
+        'categories': categories,
+        'posts': posts,
+        'comments': comments,
+    }
+    return render(request, 'community/community_management.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('community:community_management')
+    else:
+        form = CategoryForm()
+    return render(request, 'community/add_category.html', {'form': form})
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def edit_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('community:community_management')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'community/edit_category.html', {'form': form, 'category': category})
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('community:community_management')
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'community/edit_post.html', {'form': form, 'post': post})
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('community:community_management')
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'community/edit_comment.html', {'form': form, 'comment': comment})
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def delete_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    category.delete()
+    return redirect('community:community_management')
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('community:community_management')
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    comment.delete()
+    return redirect('community:community_management')
