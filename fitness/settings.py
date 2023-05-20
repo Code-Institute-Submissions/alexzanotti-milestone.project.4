9cfa454c45a18dad33525d27eb14b76c1f5ee2f7
alexzanotti <alexanderzanotti@hotmail.com>
2023-05-20 13:33:51 +0000

    cache control

--- fitness/settings.py
+++ fitness/settings.py
@@ -181,6 +181,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 23:59:59 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'milestone.project.4'
     AWS_S3_REGION_NAME = 'eu-west-2'
