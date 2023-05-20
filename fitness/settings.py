35e434674b6630e12aa7fa6c80a53776c201a1c1
alexzanotti <alexanderzanotti@hotmail.com>
2023-05-20 10:16:08 +0000

    deployment to heroku

--- fitness/settings.py
+++ fitness/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.exists("env.py"):
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-#0p9ky2(j*vm51@n(+4tg!zb1so&&nj(o37kni&z8aos739ucz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-alexzanotti-milestonepr-82nmkftc4sp.ws-eu97.gitpod.io']
+ALLOWED_HOSTS = ['https://git.heroku.com/milestone-project-4.git','localhost', '8000-alexzanotti-milestonepr-82nmkftc4sp.ws-eu97.gitpod.io']
 
 
 # Application definition
@@ -102,7 +103,7 @@ SITE_ID = 1
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -114,12 +115,19 @@ WSGI_APPLICATION = 'fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
