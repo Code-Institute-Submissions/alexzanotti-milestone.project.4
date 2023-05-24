8777759efe0f9cd8bf0595c3afba19e7500bbc8f
alexzanotti <alexanderzanotti@hotmail.com>
2023-05-24 19:28:13 +0000

    turn DEBUG to False

--- fitness/settings.py
+++ fitness/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['milestone-project-4.herokuapp.com','localhost', '8000-alexzanotti-milestonepr-82nmkftc4sp.ws-eu97.gitpod.io']
 
